172490e98a9c2bd03bc4b7f77122c24a9675a6fa
“changjingjing” <2249077862@qq.com>
2023-07-17 18:34:29 +0800

    删除无关文件

--- Python/作业/第7次作业/2060710039_昌晶晶.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-pd.read_excel(rf'./超市营业额2.xlsx')
-
-print(pd)
-
--- Python/自己/1.py
+++
@@ -1,9 +0,0 @@
-complex_one = 1 + 2j
-complex_two = 2j
-print(complex_one.real)
-print(complex_one.imag)
-print(complex_two.real)
-print(complex_two.imag)
-
-arr = [x for x in range(1, 101, 1) if x % 7 == 0 or (x > 10 and int(x / 10) == 7) or (x % 10 == 7)]
-print(arr)
