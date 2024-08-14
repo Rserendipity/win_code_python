06462dd42b1bbefc5520e055bc5395dc8c8be326
昌晶晶 <2249077862@qq.com>
2024-08-14 15:04:09 +0800

    清除文件

--- remenber_word/user/login.py
+++
@@ -1,7 +0,0 @@
-from util import mydb
-
-
-def user_login(db: mydb) -> dict:
-    user_name = input('input your user_name:>')
-    user_passwd = input('input your user_password:>')
-    return db.get_userinfo([user_name, user_passwd])
